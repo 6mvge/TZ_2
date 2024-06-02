8b3887683f9f776d468a09c357089150034e3fc4
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 12:22:02 +0300

    Check problem 3

--- src/test/java/com/test/TimeTest.java
+++ src/test/java/com/test/TimeTest.java
@@ -1,21 +1,64 @@
 package com.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import static com.test.Calculator.getSum;
 import static com.test.Calculator.getMult;
 import static com.test.Calculator.getMin;
 import static com.test.Calculator.getMax;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeTest {
 
-    public static void main(String[] args) throws IOException {
-        testPerformance("test_numbers_size_100.txt");
-        testPerformance("test_numbers_size_1000.txt");
-        testPerformance("test_numbers_size_100000.txt");
-        testPerformance("test_numbers_size_1000000.txt");
-        testPerformance("test_numbers_size_10000000.txt");
+
+    private ArrayList<Long> numbers;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        numbers =Calculator.readNumbersFromFile("test.txt");
+    }
+
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    void testGetMin() {
+        assertEquals(-101, Calculator.getMin(numbers));
+        System.out.println("testGetMin passed");
+    }
+
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    void testGetMax() {
+        assertEquals(228, Calculator.getMax(numbers));
+        System.out.println("testGetMax passed");
+    }
+
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    void testGetSum() {
+        assertEquals("280", Calculator.getSum(numbers));
+        System.out.println("testGetSum passed");
+    }
+
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    void testGetMult() {
+        assertEquals("410652290664", Calculator.getMult(numbers));
+        System.out.println("testGetMult passed");
+    }
+
+    @Disabled
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
+    void timeoutTest() throws IOException {
+        ArrayList<Long> numbers = Calculator.readNumbersFromFile("test_numbers_size_10000000.txt");
+        Calculator.getSum(numbers);
     }
 
     public static void testPerformance(String filename) throws IOException {

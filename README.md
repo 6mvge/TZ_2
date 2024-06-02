5f7d46081d08951a79528766d348abf7b9f841b7
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 16:08:37 +0300

    badge Readme.md + Timeouttest

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# TZ_2
-# TZ_2
+# ТЗ2
+
+![Java CI with Maven](https://github.com/6mvge/TZ_2/maven/actions/workflows/maven.yml/badge.svg)
--- src/test/java/com/test/TestСalculator.java
+++ src/test/java/com/test/TestСalculator.java
@@ -3,10 +3,12 @@ package com.test;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestСalculator {
 
@@ -19,34 +21,42 @@ public class TestСalculator {
     }
 
     @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     public void testGetMin() {
         assertEquals(-101, Calculator.getMin(numbers));
         System.out.println("testGetMin passed");
     }
 
     @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     public void testGetMax() {
         assertEquals(228, Calculator.getMax(numbers));
         System.out.println("testGetMax passed");
     }
 
     @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     public void testGetSum() {
         assertEquals("280", Calculator.getSum(numbers));
         System.out.println("testGetSum passed");
     }
 
     @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     public void testGetMult() {
         assertEquals("410652290664", Calculator.getMult(numbers));
         System.out.println("testGetMult passed");
     }
+
     @Test
-    @Disabled
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     public void timeoutTest() throws IOException {
         ArrayList<Long> bigSize = Calculator.readNumbersFromFile("test_numbers_size_10000000.txt");
-        Calculator.getSum(bigSize);
+        Assertions.assertThrows(AssertionError.class, () -> {
+            Assertions.assertTimeout(Duration.ofMillis(200), () -> {
+                Calculator.getSum(bigSize);
+            });
+        });
+        System.out.println("timeoutTest passed");
     }
 
 

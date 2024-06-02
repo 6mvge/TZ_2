04212cd57b9b6e8c9adeeecaeb441431e4fa7d99
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 12:18:38 +0300

    Check problem

--- src/test/java/com/test/CalculatorTest.java
+++ src/test/java/com/test/CalculatorTest.java
@@ -12,43 +12,13 @@ class CalculatorTest {
 
     private ArrayList<Long> numbers;
 
-    @BeforeEach
-    void setUp() throws IOException {
-        numbers =Calculator.readNumbersFromFile("test.txt");
-    }
 
-    @Test
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
-    void testGetMin() {
-        assertEquals(-101, Calculator.getMin(numbers));
-        System.out.println("testGetMin passed");
-    }
-
-    @Test
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
-    void testGetMax() {
-        assertEquals(228, Calculator.getMax(numbers));
-        System.out.println("testGetMax passed");
-    }
-
-    @Test
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
-    void testGetSum() {
-        assertEquals("280", Calculator.getSum(numbers));
-        System.out.println("testGetSum passed");
-    }
-
-    @Test
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
-    void testGetMult() {
-        assertEquals("410652290664", Calculator.getMult(numbers));
-        System.out.println("testGetMult passed");
-    }
 
     @Disabled
     @Test
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void timeoutTest() throws IOException {
+        System.out.println("test");
         ArrayList<Long> numbers = Calculator.readNumbersFromFile("test_numbers_size_10000000.txt");
         Calculator.getSum(numbers);
     }

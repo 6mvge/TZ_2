104500242123053ef0e2b4955912217f86cc5b49
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 12:31:17 +0300

    Check problem 4

--- src/test/java/com/test/Test.java
+++ src/test/java/com/test/Test.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+class Test {
 
     private ArrayList<Long> numbers;
 
@@ -17,40 +17,34 @@ class CalculatorTest {
         numbers =Calculator.readNumbersFromFile("test.txt");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void testGetMin() {
         assertEquals(-101, Calculator.getMin(numbers));
         System.out.println("testGetMin passed");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void testGetMax() {
         assertEquals(228, Calculator.getMax(numbers));
         System.out.println("testGetMax passed");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void testGetSum() {
         assertEquals("280", Calculator.getSum(numbers));
         System.out.println("testGetSum passed");
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     void testGetMult() {
         assertEquals("410652290664", Calculator.getMult(numbers));
         System.out.println("testGetMult passed");
     }
 
-    @Disabled
-    @Test
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
-    void timeoutTest() throws IOException {
-        ArrayList<Long> numbers = Calculator.readNumbersFromFile("test_numbers_size_10000000.txt");
-        Calculator.getSum(numbers);
-    }
+
 
 }

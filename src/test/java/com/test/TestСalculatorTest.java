dfee5abbc0d6276aeb29ff90da86ac186cb018f8
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 13:04:03 +0300

    Check problem 13

--- src/test/java/com/test/TestСalculatorTest.java
+++ src/test/java/com/test/TestСalculatorTest.java
@@ -10,37 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestСalculatorTest {
 
-    private ArrayList<Long> numbers;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        numbers = Calculator.readNumbersFromFile("test.txt");
-
-    }
-    @Test
-    void testGetMin() {
-        assertEquals(-101, Calculator.getMin(numbers));
-        System.out.println("testGetMin passed");
-    }
-
-    @Test
-    void testGetMax() {
-        assertEquals(228, Calculator.getMax(numbers));
-        System.out.println("testGetMax passed");
-    }
-
-    @Test
-    void testGetSum() {
-        assertEquals("280", Calculator.getSum(numbers));
-        System.out.println("testGetSum passed");
-    }
-
-    @Test
-    void testGetMult() {
-        assertEquals("410652290664", Calculator.getMult(numbers));
-        System.out.println("testGetMult passed");
-    }
-
     @Disabled
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     @Test

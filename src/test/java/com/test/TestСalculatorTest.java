95e8df04b478d75208c79c3c6d08d1a004a82b89
Alexandr <aatretiakov@edu.hse.ru>
2024-06-02 13:06:30 +0300

    Solution

--- src/test/java/com/test/TestСalculatorTest.java
+++ src/test/java/com/test/TestСalculatorTest.java
@@ -10,10 +10,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestСalculatorTest {
 
+    private ArrayList<Long> numbers;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        numbers = Calculator.readNumbersFromFile("test.txt");
+    }
+
+    @Test
+    public void testGetMin() {
+        assertEquals(-101, Calculator.getMin(numbers));
+        System.out.println("testGetMin passed");
+    }
+
+    @Test
+    public void testGetMax() {
+        assertEquals(228, Calculator.getMax(numbers));
+        System.out.println("testGetMax passed");
+    }
+
+    @Test
+    public void testGetSum() {
+        assertEquals("280", Calculator.getSum(numbers));
+        System.out.println("testGetSum passed");
+    }
+
+    @Test
+    public void testGetMult() {
+        assertEquals("410652290664", Calculator.getMult(numbers));
+        System.out.println("testGetMult passed");
+    }
     @Disabled
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     @Test
-    void timeoutTest() throws IOException {
+    public void timeoutTest() throws IOException {
         ArrayList<Long> bigSize = Calculator.readNumbersFromFile("test_numbers_size_10000000.txt");
         Calculator.getSum(bigSize);
     }
